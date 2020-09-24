c83d9ad5fb5920da0524773557cb6bb011fbfa80
Thanh Trúc <truc.tt7@gmail.com>
2020-09-24 22:48:46 +0700

    Delete Practive_Alert_Dismiss.rb

--- Practive_Alert_Dismiss.rb
+++
@@ -1,7 +0,0 @@
-require 'selenium-webdriver'
-driver=Selenium::WebDriver.for :chrome
-
-driver.get 'https://www.seleniumeasy.com/test/javascript-alert-box-demo.html'
-driver.find_element(xpath: "//button[@class='btn btn-default btn-lg'][contains(text(),'Click me!')]").click
-sleep 2
-driver.switch_to.alert.dismiss
