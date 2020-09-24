2b53259a087782af00e8eb03e9a62d52f743bc41
Thanh Trúc <truc.tt7@gmail.com>
2020-09-24 22:14:21 +0700

    Delete Practive_Alert.rb

--- Practive_Alert.rb
+++
@@ -1,9 +0,0 @@
-require 'selenium-webdriver'
-
-driver=Selenium::WebDriver.for :chrome
-driver.get 'https://www.seleniumeasy.com/test/javascript-alert-box-demo.html'
-driver.find_element(xpath: "//button[@class='btn btn-default']").click
-sleep 2
-driver.switch_to.alert.accept
-
-
